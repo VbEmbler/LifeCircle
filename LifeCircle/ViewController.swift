6edefc139769297af83eaf218f5382e7cb58720a
Vladimir Bondarenko <vbembler@gmail.com>
2021-02-06 20:00:56 +0300

    Initial Commit

---
+++ LifeCircle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifeCircle
+//
+//  Created by Vladimir on 06/02/2021.
+//  Copyright © 2021 Embler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
